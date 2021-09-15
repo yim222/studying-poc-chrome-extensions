f784fe60d80a72cb5e302ac676ffe4e49c658d77
lingar <yimprogramming@gmail.com>
2021-09-15 14:15:15 +0300

    should work - but the app not know it. Maybe it's ok, Need to ask them.

--- payment-page.js
+++ payment-page.js
@@ -8,20 +8,29 @@ console.log("hi admin");
 // });
 
 $(document).ready(function() {
-
+    // var e = jQuery.Event("keydown");
+    // e.which = 50; // # Some key code value
+    // $("input").trigger(e);
+    $("input[name='password']").change(function() {
+        console.log("Input text changed!");
+            alert("j")
+            $("button").trigger('click');
+    });
     $("input[name='username']").attr("value", "lingar");
     // $("input[name='password']").val("new26Way").change(function (){
     //     alert("j")
     //     $("button").trigger('click');
     //
     // });
-
+    $("input[name='password']").change();
     $("input[name='password']").attr('value', 'new26Way').on("load",function(){ //fires each time when image loaded
 
         alert("j")
         $("button").trigger('click');
 
     });
+
+
 
 
 
