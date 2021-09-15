55db86a057a4d47e400d000073ca753f3ef6e4eb
lingar <yimprogramming@gmail.com>
2021-09-15 14:00:54 +0300

    should work - but the app not know it. Maybe it's ok, Need to ask them.

---
+++ payment-page.js
@@ -0,0 +1,28 @@
+
+
+console.log("hi admin");
+//
+// $(document).ready(function(){
+//     const title = $("#question-header").children("h1").text();
+//     console.log("Welcome to practical software extension. This is stackoverflow page with -  title:\n \"" +  title + "\"");
+// });
+
+$(document).ready(function() {
+
+    $("input[name='username']").attr("value", "lingar");
+    // $("input[name='password']").val("new26Way").change(function (){
+    //     alert("j")
+    //     $("button").trigger('click');
+    //
+    // });
+
+    $("input[name='password']").attr('value', 'new26Way').on("load",function(){ //fires each time when image loaded
+
+        alert("j")
+        $("button").trigger('click');
+
+    });
+
+
+
+});
