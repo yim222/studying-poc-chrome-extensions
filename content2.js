bd8a714ee2b3ccb06e1707ef797c75fcbc3d99ac
lingar <yimprogramming@gmail.com>
2021-09-15 13:32:05 +0300

    Injecting element

--- content2.js
+++ content2.js
@@ -6,3 +6,8 @@ console.log("hi social-events");
 //     const title = $("#question-header").children("h1").text();
 //     console.log("Welcome to practical software extension. This is stackoverflow page with -  title:\n \"" +  title + "\"");
 // });
+
+$(document).ready(function() {
+    $( "<p>Test</p>" ).insertAfter( "h1" );
+
+});
