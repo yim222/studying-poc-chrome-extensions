b1720e5ef86ef209a5c7a7c7838d32d2309c7d9d
lingar <yimprogramming@gmail.com>
2021-09-17 12:25:41 +0300

    Succeed- must use pure js

--- payment-page.js
+++ payment-page.js
@@ -1,5 +1,3 @@
-
-
 console.log("hi admin");
 //
 // $(document).ready(function(){
@@ -7,31 +5,48 @@ console.log("hi admin");
 //     console.log("Welcome to practical software extension. This is stackoverflow page with -  title:\n \"" +  title + "\"");
 // });
 
-$(document).ready(function() {
+$(document).ready(function () {
     // var e = jQuery.Event("keydown");
     // e.which = 50; // # Some key code value
     // $("input").trigger(e);
-    $("input[name='password']").change(function() {
-        console.log("Input text changed!");
-            alert("j")
-            $("button").trigger('click');
-    });
-    $("input[name='username']").attr("value", "lingar");
-    // $("input[name='password']").val("new26Way").change(function (){
+    // $("input[name='password']").change(function () {
+    //     console.log("Input text changed!");
     //     alert("j")
     //     $("button").trigger('click');
-    //
     // });
-    $("input[name='password']").change();
-    $("input[name='password']").attr('value', 'new26Way').on("load",function(){ //fires each time when image loaded
+    // $("input[name='username']").attr("value", "lingar");
+    // $("input[name='username']").trigger("input");
+    // // $("input[name='password']").val("new26Way").change(function (){
+    // //     alert("j")
+    // //     $("button").trigger('click');
+    // //
+    // // });
+    // $("input[name='password']").change();
+    // $("input[name='password]'").attr('value', 'new26Way').on("load", function () { //fires each time when image loaded
+    //     $("input[name='password']").trigger("input");
 
-        alert("j")
-        $("button").trigger('click');
+// });
+    let els = document.querySelectorAll('input');
+    let event = new Event('input', { bubbles: true });
+    $("input[name='username']").attr("value", "lingar").trigger("change");
+    els[0].dispatchEvent(event);
 
-    });
+    $("input[name='password']").attr("value", "new26Way").trigger("change");
+    els[1].dispatchEvent(event);
+
+    // $("input[name='username']").attr("value", "lingar").trigger("input");
+    // $("input[name='password']").attr("value", "new26Way").trigger("input");
+    // $("input[name='username']").trigger("keypress") // you can trigger keypress like this if you need to..
+    //     .val("lingar");
+    //
+    // $("input[name='password']").trigger("keypress") // you can trigger keypress like this if you need to..
+    //     .val("new26Way");
+
+    // https://stackoverflow.com/questions/15545557/how-to-trigger-an-input-event-with-jquery/44178094
 
 
+    // els[0].setAttribute("value","lingar");
 
-
+    // els[1].setAttribute("value","new26Way");
 
 });
